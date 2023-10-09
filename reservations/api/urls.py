bf864afabc1d4ac2cb2d7c37d7cc8d3fbcfdfe8b
Anıl Pirinççi <anilpirincciii@gmail.com>
2023-10-09 22:07:46 +0300

    project_created

---
+++ reservations/api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ReservationsViewSet, ReservationCreateAPIView, ReservationAvailabilityAPIView, ReservationDetailView, ReservationsListAll, ReservationDeleteView, ReservationUpdateView
+
+
+router = DefaultRouter()
+router.register(r'crud', ReservationsViewSet, basename="reservationviewset")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("add/auth", ReservationCreateAPIView.as_view(), name="car_user_add_reserv"),
+    path("add/", ReservationCreateAPIView.as_view(), name="car_add_reserv"),
+    path("auth/", ReservationAvailabilityAPIView.as_view(), name="reserv_availability"),
+    path("<int:pk>/auth/", ReservationDetailView.as_view(), name="reserv_detail_auth"),
+    path("<int:pk>/admin/", ReservationDetailView.as_view(), name="reserv_detail_admin"),
+    path("admin/all", ReservationsListAll.as_view(), name="list_admin_all"),
+    path("auth/all", ReservationsListAll.as_view(), name="list_auth"),
+    path("admin/auth/all", ReservationsListAll.as_view(), name="list_auth_admin"),
+    path("admin/all/pages/", ReservationsListAll.as_view(), name="list_pages_admin"),
+    path("admin/<int:pk>/auth", ReservationDeleteView.as_view(), name="reserv_delete"),
+    path("admin/auth", ReservationUpdateView.as_view(), name="reserv_delete"),
+]
